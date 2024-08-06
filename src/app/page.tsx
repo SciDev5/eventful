5320138e23f510dd744316d25b95512ac9371ce3
SciDev5 <skhuber0@gmail.com>
2024-08-05 20:44:29 -0400

    fix useSearchParam requires Suspense issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,20 @@
 
 import styles from "./page.module.css";
 import { useAwait } from "@/util/use";
-import { useMemo } from "react";
+import { Suspense, useMemo } from "react";
 import { EventViewer, EventViewerDataMissing } from "@/components/events/EventViewer";
 import { useSearchParams } from "next/navigation";
 import { getdata_by_id } from "@/data/fetcher";
 
 export default function Home() {
+    return (
+        <Suspense fallback={<HomeFallback />}>
+            <HomeLoaded />
+        </Suspense>
+    );
+}
+
+function HomeLoaded() {
     const params = useSearchParams()
     const event_id = params.get("event") ?? "rex" // event info source, current major event is rex so default to that
     const is_embed = params.get("is_embed") != null
@@ -25,6 +33,16 @@ export default function Home() {
                         ? <EventViewer schedule={event_schedule} />
                         : <EventViewerDataMissing failed={event_schedule === "failed" ? event_id : false} />
                 }
+            </div>
+        </main >
+    );
+}
+function HomeFallback() {
+    return (
+        <main className={styles.main}>
+            {<h2 style={{ padding: "0.6em 1.0em", color: "#aaf9", textWrap: "nowrap" }}>:: EVENTFUL timeline ::</h2>}
+            <div style={{ position: "absolute", left: 0, right: 0, top: "3em", bottom: 0 }}>
+                <EventViewerDataMissing failed={false} />
             </div>
         </main >
     );
